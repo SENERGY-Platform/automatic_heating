c7a0bd25aba5ea59e686257893c04726660a8253
c-schrade <schrade.christoph@gmail.com>
2025-11-16 17:07:12 +0100

    compute confidences for days in the past for debugging

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ class Operator(OperatorBase):
             logger.debug(f"Historic data from: {current_timestamp}:  Window open: {window_open}!")
 
 
-        if window_open:
+        if window_open and real_time_data:
             self.window_opening_times = load(self.data_path, WINDOW_OPENING_TIMES_FILE, default=[])
             self.window_opening_times.append(current_timestamp)
             save(self.data_path, WINDOW_OPENING_TIMES_FILE, self.window_opening_times)
@@ -127,7 +127,7 @@ class Operator(OperatorBase):
 
 
         confidence_list = []
-        if new_day and real_time_data:
+        if new_day:
             window_opening_times = load(self.data_path, WINDOW_OPENING_TIMES_FILE, default=[])
             clusters_boundaries = compute_clusters_boundaries(window_opening_times)
             current_day = current_timestamp.floor("d")
