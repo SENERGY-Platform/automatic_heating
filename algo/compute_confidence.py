fix confidence computation

algo/compute_confidence.py
@@ -11,8 +11,8 @@ def compute_second_momentum(list_of_ts:list):
     momentum = np.sqrt(non_avg_momentum/len(list_of_frac))
     return momentum*24*3600
 
-def compute_confidence_from_spreading(list_of_ts:list, high_confidence_boundary: float, low_confidence_boundary: float):
-    second_momentum = compute_second_momentum(list_of_ts)
+def compute_confidence_from_spreading(ts_in_cluster:list, high_confidence_boundary: float, low_confidence_boundary: float):
+    second_momentum = compute_second_momentum(ts_in_cluster)
     confidence = -1/(low_confidence_boundary - high_confidence_boundary)*second_momentum + 1+(high_confidence_boundary)/(low_confidence_boundary-high_confidence_boundary)
     if confidence >= 1:
         confidence = 1

algo/create_clustering.py
@@ -29,18 +29,20 @@ def compute_clustering(window_opening_times: list):
 
     clustering = DBSCAN(eps=EPSILON, min_samples=2).fit(projections_onto_circle)
     clusters = {}
+    indices = {}
     for c in np.unique(clustering.labels_):
         ix = np.where(clustering.labels_ == c)
+        indices[c] = ix[0]
         clusters[c] = [window_opening_times[i] for i in ix[0]]
-    return clusters
+    return clusters, indices
 
 def compute_clusters_boundaries(window_opening_times: list):
-    clusters = compute_clustering(window_opening_times)
+    clusters, indices = compute_clustering(window_opening_times)
     clusters_boundaries = {}
     for c in clusters.keys():
         cluster_minimum = min(map(lambda x: x.time(), clusters[c]))
         cluster_maximum = max(map(lambda x: x.time(), clusters[c]))
         clusters_boundaries[c] = (cluster_minimum, cluster_maximum)
 
-    return clusters_boundaries
+    return clusters_boundaries, indices
 